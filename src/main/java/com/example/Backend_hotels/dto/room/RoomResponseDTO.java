b2c0e2ef360acbb599e4b736e373f8bc74ce336b
João Alexandre <joaoalekirst@hotmail.com>
2026-04-04 14:24:46 -0300

    Coreção do gets e setters do RoomResponseDTO

--- src/main/java/com/example/Backend_hotels/dto/room/RoomResponseDTO.java
+++ src/main/java/com/example/Backend_hotels/dto/room/RoomResponseDTO.java
@@ -27,5 +27,67 @@ public class RoomResponseDTO {
         this.hotelId = hotelId;
     }
 
-    // getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getBeds() {
+        return beds;
+    }
+
+    public void setBeds(Integer beds) {
+        this.beds = beds;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Boolean availability) {
+        this.availability = availability;
+    }
+
+    public Long getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Long hotelId) {
+        this.hotelId = hotelId;
+    }
 }
